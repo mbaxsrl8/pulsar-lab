48722c97f405fbbc106128f36d47483c88a48d9a
LU RUI <a10115307@gmail.com>
2025-01-06 22:23:30 +0800

    add scripts

---
+++ scripts/pulsar.bat
@@ -0,0 +1,1 @@
+docker run -d -p 6650:6650 -p 8080:8080 -e PULSAR_STANDALONE_USE_ZOOKEEPER=1 --mount source=pulsardata,target=/pulsar/data --mount source=pulsarconf,target=/pulsar/conf apachepulsar/pulsar:4.0.0 bin/pulsar standalone
