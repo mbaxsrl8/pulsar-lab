c0f2f192b4c65dded330b850f856effee5c548c1
LU RUI <a10115307@gmail.com>
2025-01-06 22:24:49 +0800

    use simple provider slf4j

---
+++ aimless-producer/src/main/java/SampleProducer.java
@@ -0,0 +1,26 @@
+import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.PulsarClient;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.api.Schema;
+
+import java.util.logging.Logger;
+
+public class SampleProducer {
+    private static final Logger LOGGER = Logger.getLogger(SampleProducer.class.getName());
+    public static void main(String[] args) {
+        try (PulsarClient client = PulsarClient.builder()
+                .serviceUrl("pulsar://127.0.0.1:56726")
+                .build();
+             Producer<String> stringProducer = client.newProducer(Schema.STRING)
+                     .topic("my-topic")
+                     .create();) {
+
+            stringProducer.send("My message");
+            LOGGER.info("Sent message");
+
+        } catch (PulsarClientException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
